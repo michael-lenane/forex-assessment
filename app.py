dd90dfcd20a34dab1b93429869c2f3fd5dc2cae4
Michael Lenane <michael.j.lenane@gmail.com>
2022-07-06 14:21:55 -0400

    installed and imported python-forex currency converter into app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+from forex_python.converter import CurrencyCodes
+from forex_python.converter import CurrencyRates
 
 
 app = Flask(__name__)
