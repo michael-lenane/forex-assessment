bf00077f68083ca7d565a8faaa417350e85d8e86
Michael Lenane <michael.j.lenane@gmail.com>
2022-07-06 14:55:20 -0400

    created first test template and created config secret key

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ from forex_python.converter import CurrencyRates
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
+
+
+@app.route("/test")
+def show_test_page():
+
+    return render_template("base.html")
