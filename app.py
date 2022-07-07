1e3f5637ae59fea3c303387bce037cacabda5180
Michael Lenane <michael.j.lenane@gmail.com>
2022-07-07 16:01:07 -0400

    modified route to allow users to select from a drop down of currencies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 from forex_python.converter import CurrencyCodes
 from forex_python.converter import CurrencyRates
@@ -8,11 +8,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
-currency_codes = CurrencyCodes()
+
 currency_rates = CurrencyRates()
 
 
-@app.route("/test")
+@app.route("/convert")
 def show_test_page():
 
-    return render_template("base.html")
+    currencies = ["EUR", "IDR", "BGN", "ILS", "GBP", "DKK", "CAD", "JPY", "HUF", "RON", "MYR", "SEK", "SGD", "HKD", "AUD",
+                  "CHF", "KRW", "CNY", "TRY", "HRK", "NZD", "THB", "USD", "NOK", "RUB", "INR", "MXN", "CZK", "BRL", "PLN", "PHP", "ZAR"]
+
+    currency_codes = CurrencyCodes()
+
+    global currency_value
+    currency_value = request.form.get('start')
+
+    return render_template("convert.html", currencies=currencies, currency_codes=currency_codes, currency_value=currency_value)
